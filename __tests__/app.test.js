02ec66671d73d6e579b30d0c4739c8ca288f035b
Matt Munch <mattmunch94@gmail.com>
2019-12-18 15:58:21 -0800

    couple routes tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Crash = require('../lib/models/Crash');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -13,8 +14,53 @@ describe('app routes', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  
+  let crash;
+  beforeEach(async() => {
+    crash = await Crash.create({
+      eventDescriptor:'Deer',
+      crashDescriptor:'Things happened',
+      lightingCondition:'Broad Daylight',
+      numberOfVehiclesInvolved:99,
+    });
+  });
 
   afterAll(() => {
     return mongoose.connection.close();
   });
+  it('can create a crash', () => {
+    return request(app)
+      .post('/api/v1/crashes')
+      .send({
+        eventDescriptor:'Deer',
+        crashDescriptor:'Things happened',
+        lightingCondition:'Broad Daylight',
+        numberOfVehiclesInvolved:99,
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          eventDescriptor:'Deer',
+          crashDescriptor:'Things happened',
+          lightingCondition:'Broad Daylight',
+          numberOfVehiclesInvolved: 99,
+          __v:0
+        });
+      });
+  });
+  it('can get a crash by id', () => {
+    return request(app)
+      .get(`/api/v1/crashes/${crash.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: crash.id,
+          eventDescriptor:'Deer',
+          crashDescriptor:'Things happened',
+          lightingCondition:'Broad Daylight',
+          numberOfVehiclesInvolved: 99,
+          __v:0
+        });
+      });
+  });
+
 });
---
+++ seed.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+require('./lib/utils/connect')();
+const mongoose = require('mongoose');
+const { seedData } = require('./lib/utils/seed-data');
+seedData()
+  .then(() => console.log('done'))
+  .finally(() => mongoose.connection.close());
